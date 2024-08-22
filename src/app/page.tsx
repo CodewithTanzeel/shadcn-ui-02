3a4ef003f7a51c185db42d080984cf35c05f19f1
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-23 00:09:23 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
     
     <div className="text-center p-5  text-white bg-emerald-900">
-      <h1 className=" text-2xl "> Analysis</h1>
+      <h1 className=" text-2xl "> Shadcn ui</h1>
       <div>
         <AccordionComp/>
       </div>
