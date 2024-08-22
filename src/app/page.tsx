4efc7d6aa7abf9b8111ad41749214a3c61ff5aea
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-23 00:08:21 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import AccordionComp from "./accordion";
 export default function Home() {
   return (
 
-    <main className="bg-green-400">
-    <div className="text-center p-5  text-white">
-      <h1 className=" text-2xl "> Shadcn ui</h1>
+    
+    <div className="text-center p-5  text-white bg-emerald-900">
+      <h1 className=" text-2xl "> Analysis</h1>
       <div>
         <AccordionComp/>
       </div>
     </div>
-    </main>
+   
 
 
   );
