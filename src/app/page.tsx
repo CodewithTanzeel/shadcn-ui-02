957331fed0b6e2a102e033763ccc19b4c6b3d5ea
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-24 11:52:46 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,11 @@ export default function Home() {
     
     <div className="text-center p-10  text-white bg-emerald-900 w-screen h-full justify-center">
       <h1 className=" text-2xl "> Shadcn ui</h1>
-      
+      {/* Main heading H1 */}
         <AccordionComp />
         {/* Rendering a component same by help of nesting elements of accordion in a function  */}
         <AlertDialogDemo/>
+        {/* Rendering a component same by help of nesting elements of accordion in a function  */}
       
     </div>
    
