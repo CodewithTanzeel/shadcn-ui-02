c131993404a809e49ae18061bacef345a149e4ae
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-25 23:19:10 +0500

    update

--- src/app/alertdialog.tsx
+++ src/app/alertdialog.tsx
@@ -9,18 +9,23 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"//Here we are importing button component from root ui 
 
 
   import React from 'react'
 import InputDemo from "./input"
+ 
+
+// we made a funtion here to nest the content of alert Dialog component
   
   const AlertDialogDemo = () => {
     return (
       <div className="p-5 flex-col pt-45">
         <AlertDialog >
   <AlertDialogTrigger>
-    <Button className="bg-amber-800">More..</Button>
+    
+  <Button className="bg-amber-800">More..</Button>
+    {/* Here we are using the button component in this case we are nesting it in the component itself */}
   </AlertDialogTrigger>
   <AlertDialogContent>
     <AlertDialogHeader>
